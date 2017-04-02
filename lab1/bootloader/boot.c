957790d85cec135030d7476b7ad5609ed19a7896
newuhe <2383766952@qq.com>
2017-04-02 21:29:55 +0800

    Load app from disk to ram

---
+++ lab1/bootloader/boot.c
@@ -0,0 +1,29 @@
+#include "boot.h"
+#define SECTSIZE 512
+#define START_ADDR 0x8c00
+void bootmain(void) {
+    	void(*elf)(void)=(void*)START_ADDR;
+	readSect((void *)START_ADDR,1);
+	elf();
+}
+
+void waitDisk(void) { // waiting for disk
+	while((inByte(0x1F7) & 0xC0) != 0x40);
+}
+
+void readSect(void *dst, int offset) { // reading one sector of disk
+	int i;
+	waitDisk();
+	outByte(0x1F2, 1);
+	outByte(0x1F3, offset);
+	outByte(0x1F4, offset >> 8);
+	outByte(0x1F5, offset >> 16);
+	outByte(0x1F6, (offset >> 24) | 0xE0);
+	outByte(0x1F7, 0x20);
+
+	waitDisk();
+	for (i = 0; i < SECTSIZE / 4; i ++) {
+		((int *)dst)[i] = inLong(0x1F0);
+	}
+}
+
